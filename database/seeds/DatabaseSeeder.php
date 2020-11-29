bc3ed0c3688aa34b33445af1b2521c37beafc1a5
Gianfranco <gmeneguz@gmail.com>
2020-11-29 16:55:13 -0300

    feature: add Genre resource

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call(CategorySeeder::class);
+      $this->call(GenreSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
