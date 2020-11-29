b44d48ad0a2a951e8ce463c0051f20391a96fdf0
Gianfranco <gmeneguz@gmail.com>
2020-11-29 19:02:07 +0000

    feature: Create Category resource

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,10 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
+
+Route::group(['namespace' => 'Api'],function(){
+  Route::resource('categories', 'CategoryController',['except' => ['create','edit']]);
+});
+
